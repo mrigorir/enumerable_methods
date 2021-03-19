9e68c07cb266cab1750ed63192f12a98eb9c36f8
Marco <marcoparra311@gmail.com>
2021-03-19 13:03:10 -0400

    Make changes on #my_each method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,18 +1,8 @@
 module Enumerable
   def my_each
-    Enumerator.new(self) unless block_given?
-
-    for i in 0...self.length
-      if self.is_a Hash
-      yield self[i]
-      yield self[i]
-      elsif self.is_a Array
-        yield self[i]
-      end
+    return Enumerable.new(self) unless block_given?
+    for i in 0...to_a.length
+      yield to_a[i]
     end
   end
 end
-
-words = {:word1 => 'string1', :word2 => 'string2', :word3 => 'string3'}
-
-words.my_each { |k, v| p "key: #{k}, value: #{v}"}
