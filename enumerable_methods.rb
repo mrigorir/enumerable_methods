83ef921714d0843ae6d990dda79b6a436cfbfb43
Marco <marcoparra311@gmail.com>
2021-03-19 15:20:16 -0400

    Implement #my_each_with_index and #my_select methods

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,8 +1,40 @@
 module Enumerable
   def my_each
-    return Enumerable.new(self) unless block_given?
-    for i in 0...to_a.length
+    return Enumerator.new(self) unless block_given?
+
+    for i in 0...length
       yield to_a[i]
     end
   end
+
+  def my_each_with_index
+    return Enumerator.new(self) unless block_given?
+
+    for i in 0...self.length
+      yield to_a[i], i
+    end
+  end
+  def my_select
+    return Enumerator.new(self) unless block_given?
+
+    selected_el = Array.new
+    my_each { |sel| selected_el << sel if yield sel }
+    selected_el
+  end
 end
+
+words = {word1: 'string1', word2: 'string2', word3: 'string3'}
+
+array = [1, 2, 3]
+
+friends = ['Marco', 'Adriana', 'Felon']
+
+words.my_each { |k, v| p "key: #{k} value: #{v}" }
+
+array.my_each_with_index { |el, i|  puts "el: #{el} index: #{i+1}" } 
+
+p array.my_select { |n| n > 1}
+
+p friends.my_select { |f| f != 'Felon'}
+
+p words.my_select { |w, s| s == 'string1' }
