516bbb21ffd86f8ddf11f629e8701772a1eb444c
Arthur Borges <arthurborges.dev@gmail.com>
2021-03-19 18:13:50 -0300

    Add #my_any? method to the list of methods. Fix the length of arguments check in #my_all? method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -30,23 +30,46 @@ module Enumerable
       check = true
       my_each { |el| check = false if yield(el) == false }
       return check
-    elsif method(:my_all?).parameters.size > 0 # have argument - for matching
+    elsif args.size > 0 # have argument - for matching
       check = true
       if args[0].class == Regexp
         my_each { |el| check = false unless el === args[0] }
         return check
       else
-        my_each { |el| check = false unless el.class == args[0] }
+        my_each { |el| check = false unless [el.class, el.class.superclass].include?(args[0]) }
         return check
       end 
     else
       check = true
-      my_each { |el| check = false if el == nil || el == false }
+      my_each do |el| 
+        check = false if el == nil || el == false 
+      end
       check
     end
   end
 
-  
+  def my_any?(*args)
+    if block_given?
+      check = false
+      my_each { |el| check = true if yield(el) == true }
+      return check
+    elsif args.size > 0 # have argument - for matching
+      check = false
+      if args[0].class == Regexp
+        my_each { |el| check = true if el === args[0] }
+        return check
+      else
+        my_each { |el| check = true if [el.class, el.class.superclass].include?(args[0]) }
+        return check
+      end 
+    else
+      check = 0
+      my_each { |el| check += 1 if el == true }
+      check > 0 ? true : false
+    end
+  end
+
+
 end
 
 words = {word1: 'string1', word2: 'string2', word3: 'string3'}
@@ -60,9 +83,20 @@ friends = ['Marco', 'Adriana', 'Felon']
 #p words.my_select { |w, s| s == 'string1' }
 #p [1, 5, 7].each { |el| p el * 10 }
 
-#p %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
-#p %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
-#p %w[ant bear cat].my_all?(/t/)                        #=> false
-#p [1, 2i, 3.14].my_all?(Numeric)                       #=> true
-#p [nil, true, 99].my_all?                              #=> false
-#p [].my_all?                                           #=> true
+
+p %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
+p %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+p %w[ant bear cat].my_all?(/t/)                        #=> false
+p [1, 2i, 3.14].my_all?(Numeric)                       #=> true
+p [nil, true, 99].my_all?                              #=> false
+p [].my_all?                                           #=> true
+
+
+p "---------------------"
+
+p %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
+p %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
+p %w[ant bear cat].my_any?(/d/)                        #=> false
+p [nil, true, 99].my_any?(Integer)                     #=> true
+p [nil, true, 99].my_any?                              #=> true
+p [].my_any?                                           #=> false
