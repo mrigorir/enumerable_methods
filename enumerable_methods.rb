656c8f17cf1982a5bfb94cf1a81a6abcce665b35
Arthur Borges <arthurborges.dev@gmail.com>
2021-03-19 16:36:06 -0300

    Fix return value of #my_each and #my_each_with_index changing it to self

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -5,6 +5,7 @@ module Enumerable
     for i in 0...self.length
       yield to_a[i]
     end
+    self
   end
 
   def my_each_with_index
@@ -13,13 +14,20 @@ module Enumerable
     for i in 0...self.length
       yield to_a[i], i
     end
+    self
   end
+
   def my_select
-    return self._enum unless block_given?
+    return self.to_enum unless block_given?
 
     selected_el = Array.new
     my_each { |sel| selected_el << sel if yield sel }
     selected_el
+  end
+
+  def my_all?
+    return self.to_enum unless block_given?
+
   end
 end
 
@@ -29,12 +37,15 @@ array = [1, 2, 3]
 
 friends = ['Marco', 'Adriana', 'Felon']
 
-words.my_each { |k, v| p "key: #{k} value: #{v}" }
+#p words.my_each { |k, v| p "key: #{k} value: #{v}" }
 
-array.my_each_with_index { |el, i|  puts "el: #{el} index: #{i+1}" } 
+#p array.my_each_with_index { |el, i|  puts "el: #{el} index: #{i+1}" } 
+# ------------------
+#p array.my_select { |n| n > 1}
 
-p array.my_select { |n| n > 1}
+#p friends.my_select { |f| f != 'Felon'}
 
-p friends.my_select { |f| f != 'Felon'}
+#p words.my_select { |w, s| s == 'string1' }
 
-p words.my_select { |w, s| s == 'string1' }
+
+#p [1, 5, 7].each { |el| p el * 10 }
